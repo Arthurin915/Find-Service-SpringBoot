10e3db1c58511cedb43036a4d3e65713aa655a36
Arthur Inda <arthurindarocha1@hotmail.com>
2017-05-16 01:38:43 -0300

    Mudanças no input

--- src/main/resources/static/js/index.js
+++
@@ -1,9 +0,0 @@
-$('#InputUsuarioConfirmar').blur(function(){
-	if($('#InputUsuarioConfirmar').val() == $('#InputUsuarioSenha').val()){
-		alert('Certo');
-		$('#btn-salvar-usuario').attr('disabled',false);
-	}else {
-		$('#btn-salvar-usuario').attr('disabled',true);
-	}
-});
-
